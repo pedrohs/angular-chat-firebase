b1cecd9ccf36781e32acd05f05025166693753d9
Pedro Henrique <9.pedro.deoliveira@gmail.com>
2016-01-01 14:04:57 -0200

    alerta no title sobre novas msgs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('appChat', ['firebase']);
 
-app.controller('chatCtrl', function($scope, $firebaseArray){
+app.controller('chatCtrl', function($scope, $firebaseArray, $window){
 	var ref = new Firebase("https://tvchat.firebaseio.com");
 	
 	$scope.error = false;
@@ -8,18 +8,20 @@ app.controller('chatCtrl', function($scope, $firebaseArray){
 
 	$scope.login = function(){
 		ref.authWithOAuthPopup("facebook", function(error, authData) {
-		if (error) {
-			$scope.error = true;
-		} else {
-			$scope.$apply(function(){
-				console.log(authData.facebook);
+			if (error) {
+				$scope.error = true;
+			} else {
+				$scope.$apply(function(){
+					console.log(authData.facebook);
 
-				$scope.mensagens = $firebaseArray(ref);
-				var data = authData.facebook;
-				$scope.user = {id: data.id, name: data.displayName, picture: data.profileImageURL};
-			});
-		}
-	});
+					$scope.mensagens = $firebaseArray(ref);
+					var data = authData.facebook;
+					$scope.user = {id: data.id, name: data.displayName, picture: data.profileImageURL};
+
+					notification();
+				});
+			}
+		});
 	};
 
 	$scope.enviar = function(e){
@@ -27,10 +29,50 @@ app.controller('chatCtrl', function($scope, $firebaseArray){
 			if($scope.mensagem.length >= 1){
 				$scope.user.mensagem = $scope.mensagem;
 				$scope.mensagens.$add($scope.user).then(function(){
-				$scope.mensagem = "";
-			});
+					$scope.mensagem = "";
+				});
 			}
 			
 		}
+	};
+
+	function notification(){
+		var qntMensagens = 0;
+		$window.onblur = function(){
+			$scope.mensagens.$watch(function(){
+				qntMensagens++;
+				$scope.titleAlert = "(" + qntMensagens + ")";
+			});
+			
+		};
+		$window.onfocus = function(){
+			$scope.$apply(function(){
+				qntMensagens = 0;
+				$scope.titleAlert = "";
+			});
+
+		};
 	}
+	/*
+	function notification(){
+		var qntMensagens = 0;		
+		$scope.mensagens.$watch(function(){
+
+			$window.onblur = function(){
+				$scope.$apply(function(){
+					qntMensagens++;
+					$scope.titleAlert = "(" + qntMensagens + ")";
+				});
+				
+			};
+			$window.onfocus = function(){
+				$scope.$apply(function(){
+					qntMensagens = 0;
+					$scope.titleAlert = "";
+				});				
+			};
+		});
+
+	};
+	*/
 });
